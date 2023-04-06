d94b102c9b2a401114b1e44fb4b64e5099350480
popyoung <youngpop@163.com>
2023-04-06 09:53:49 +0800

    1.修改配置文件的方案，改为嵌入deck options 2.刷新复习和学习单词数量 3.修正可能有空操作撤销的情况

--- BuryNdays.py
+++ BuryNdays.py
@@ -2,18 +2,20 @@ from aqt import mw
 import time
 from aqt.utils import tooltip
 from aqt import gui_hooks
+from pathlib import Path
+import json
 
-config = mw.addonManager.getConfig(__name__)
 now = time.time()
-buryInterval = config.get("buryInterval", 1)
 
-def autobury(self, c):
-    # pprint(dir(mw.col.sched))
+
+def autobury(self):
+    # pprint(dir(mw.col.decks))
+    # print(mw.col.decks.current()['id'])
+    # print(c)
     toBury = set()
     checked = set()
     count = 0
-    mw.col.add_custom_undo_entry("Undo auto bury")
-    undostep = mw.col.undo_status().last_step
+    undostep = -1
     for i in range(10000):
         # mw.col.reset()
         cards = mw.col.sched.get_queued_cards(fetch_limit=10000).cards
@@ -27,6 +29,7 @@ def autobury(self, c):
                 did = card.odid
             newbury = mw.col.decks.config_dict_for_deck_id(did)["new"].get("bury", True)
             revbury = mw.col.decks.config_dict_for_deck_id(did)["rev"].get("bury", True)
+            buryInterval = mw.col.decks.config_dict_for_deck_id(did).get("buryInterval", 3)
             if ((card.queue == 0 and newbury) or (card.queue == 2 and revbury)):
                 for cid, queue in mw.col.db.execute(
                         f"""select id, queue from cards where nid=? and id!=?""",
@@ -39,12 +42,32 @@ def autobury(self, c):
         # print( len(toBury))
         if (len(toBury) == 0):
             break
+        if (undostep < 0):
+            mw.col.add_custom_undo_entry("Undo auto bury")
+            undostep = mw.col.undo_status().last_step
         mw.col.sched.bury_cards(toBury, manual=False)
         count += len(toBury)
         toBury.clear()
     # print(checked)
-    mw.col.merge_undo_entries(undostep)
-    mw.update_undo_actions()
-    tooltip("bury " + str(count) + " card(s)")
+    if (count > 0):
+        mw.col.merge_undo_entries(undostep)
+        mw.update_undo_actions()
+        self.refresh()
+        tooltip("bury " + str(count) + " card(s)")
 
-gui_hooks.overview_will_render_content.append(autobury)
+gui_hooks.overview_did_refresh.append(autobury)
+
+file = Path(__file__)
+
+with open(file.with_name("raw.html"), encoding="utf8") as f:
+    html = f.read()
+with open(file.with_name("raw.js"), encoding="utf8") as f:
+    script = f.read()
+
+
+def optionInject(deck_options):
+    # print(dir(deck_options))
+    deck_options.web.eval(script.replace("HTML_CONTENT", json.dumps(html)))
+
+
+gui_hooks.deck_options_did_load.append(optionInject)
